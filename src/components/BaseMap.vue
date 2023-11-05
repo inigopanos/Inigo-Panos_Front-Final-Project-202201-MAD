ec6c785a29ec0994bbf7dcac65535b03139658cf
Iñigo <inigopanos@gmail.com>
2023-11-05 19:33:23 +0100

    Añadido a mounted el valor de isLoading y userLocation de userPlacesStore.

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -1,33 +1,42 @@
 <template>
   <head>
-
-    <p>1: {{ userLocation }}, 2: {{ isLoading }}</p>
   </head>
+  <p>1: {{ userLocation }}, </p>
+  <p> 2: {{ isLoading }},</p>
+  <p>3: {{ prueba1 }}</p>
   <div id="map"></div>
 </template>
 
 <script lang="ts">
   import 'mapbox-gl/dist/mapbox-gl.css';
-  import {mapState, mapGetters} from 'vuex';
-  import { computed, defineComponent } from 'vue';
+  import {mapActions, mapState, mapGetters} from 'vuex';
+  import { defineComponent } from 'vue';
   import {userPlacesStore} from '../router/places.service'
+
 
 export default defineComponent({
   components: { },
-  // data() {
-  //   return {
-  //     userLocation: [Number, Number],
-  //     isLoading: Boolean,
-  //   };
-  // },
+  data() {
+    return {
+      prueba1: ""
+    };
+  },
 
   computed:{
     ...mapGetters('places', ['isUserlocationReady']),
-    ...mapState(['places']),
+    ...mapState(['store']),
+  },
+
+  methods:{
+    ...mapActions('places', [
+      'isUserlocationReady'
+    ])
   },
 
   setup() {
     const { isLoading, userLocation,} = userPlacesStore();
+
+    console.log('Polla', isLoading, userLocation);
 
     return { 
       isLoading,
@@ -35,9 +44,9 @@ export default defineComponent({
     }
   },
 
-  mounted(){
-    this.isUserlocationReady();
-    console.log(this.isLoading, this.userLocation);
+  mounted() {
+    const { isLoading, userLocation } = userPlacesStore();
+    console.log('Prueba1:', isLoading, userLocation);
   }
 
 });
