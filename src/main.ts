6770af5b9ab298820ecff08fd7d1dc1e9114ff67
Iñigo <inigopanos@gmail.com>
2023-11-28 12:04:38 +0100

    Quitado clog de token.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import './registerServiceWorker';
 import router from './router';
 import { store } from './store/index';
 
-console.log('Mapbox token:', process.env.VUE_APP_MAPBOX_TOKEN);
 mapboxgl.accessToken = "pk.eyJ1IjoiaW5pZ29wYW5vcyIsImEiOiJjbHA2enUzcTcyOGprMmlycHphbnlvMG9lIn0.YdZs7aTRm-5F1Q5zz0wqDw";
 
 createApp(App).use(store).use(router).use(VueLazyload).mount('#app');
