319cbb4c24da078cfce0058da87aed8b37957b78
Iñigo <inigopanos@gmail.com>
2023-11-29 10:03:07 +0100

    Añadido token de acceso de mapboxgl como .env var

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue';
 import mapboxgl from 'mapbox-gl';
 import VueLazyload from 'vue-lazyload'
+import * as dotenv from 'dotenv';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import { store } from './store/index';
 
-mapboxgl.accessToken = "pk.eyJ1IjoiaW5pZ29wYW5vcyIsImEiOiJjbHA2enUzcTcyOGprMmlycHphbnlvMG9lIn0.YdZs7aTRm-5F1Q5zz0wqDw";
+dotenv.config();
+
+mapboxgl.accessToken = process.env.accessToken as string;
 
 createApp(App).use(store).use(router).use(VueLazyload).mount('#app');
